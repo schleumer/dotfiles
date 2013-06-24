748fb3a83fbbaa25ba421bcbcc49c99c2d817249
Wesley Schleumer <wesley.schleumer@gmail.com>
2013-06-23 23:00:49 -0300

    nice stuffs

---
+++ my.zsh
@@ -0,0 +1,42 @@
+stty ixany
+stty ixoff -ixon
+
+
+function cd(){
+	echo "i ain't paid for that."
+	builtin cd "$@"
+}
+
+
+function svnaddall(){
+	svn st | grep "^\?" | awk "{print \$2}" | xargs svn add $1
+}
+
+function svnrmall(){
+	svn st | grep '^!' | awk '{print $2}' | xargs svn delete --force
+}
+
+function download(){
+	curl --remote-name $1
+}
+
+function extract () {
+   if [ -f $1 ] ; then
+       case $1 in
+           *.tar.bz2)   tar xvjf $1 && cd $(echo $1 | sed 's/.tar.bz2//')    ;;
+           *.tar.gz)    tar xvzf $1 && cd $(echo $1 | sed 's/.tar.gz//')    ;;
+           *.bz2)       bunzip2 $1 && cd $(echo $1 | sed 's/.bz2//')    ;;
+           *.rar)       unrar x $1 && cd $(echo $1 | sed 's/.rar//')    ;;
+           *.gz)        gunzip $1 && cd $(echo $1 | sed 's/.gz//')    ;;
+           *.tar)       tar xvf $1 && cd $(echo $1 | sed 's/.tar//')    ;;
+           *.tbz2)      tar xvjf $1 && cd $(echo $1 | sed 's/.tbz2//')    ;;
+           *.tgz)       tar xvzf $1 && cd $(echo $1 | sed 's/.tgz//')    ;;
+           *.zip)       unzip $1 && cd $(echo $1 | sed 's/.zip//')    ;;
+           *.Z)         uncompress $1 && cd $(echo $1 | sed 's/.Z//')    ;;
+           *.7z)        7z x $1 && cd $(echo $1 | sed 's/.7z//')    ;;
+           *)           echo "don't know how to extract '$1'..." ;;
+       esac
+   else
+       echo "'$1' is not a valid file!"
+   fi
+ }
