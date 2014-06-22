74efb01708558b32270c510049bf44a4c5d11761
Wesley Schleumer <wesley.schleumer@gmail.com>
2014-06-22 19:32:51 -0300

    kek

--- utils/kde-bootstrap.sh
+++ utils/kde-bootstrap.sh
@@ -4,3 +4,6 @@ synclient FingerHigh=5
 synclient CoastingSpeed=0
 synclient MinSpeed=1.75
 synclient MaxSpeed=3
+synclient VertTwoFingerScroll=0
+synclient HorizTwoFingerScroll=0
+synclient VertEdgeScroll=1
